c989614d37a9e2647e6060fb80bb10818eb70377
Alexandre Franca <basic4@gmail.com>
2021-08-07 13:21:54 +0100

    JavaTPoint Original Code

--- src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.javatpoint.microservices.currencyconversionservice") 
+@EnableFeignClients("com.javatpoint.microservices.currencyconversionservice")
 @EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
-
 }
